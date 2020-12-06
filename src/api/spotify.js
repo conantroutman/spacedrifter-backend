a1b751dbacdb5a4c5bff3c30e198386db1371335
John Söderberg <johsod96@gmail.com>
2020-12-06 01:09:27 +0100

    Access token is refreshed automatically if expired

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const SpotifyWebApi = require('spotify-web-api-node');
 
+// The artist ID to get albums from
+const artistId = '4otyLOpxTJ6VdY0EEfjIcS';
+let refreshTimer = Date.now();
+
 // Spotify API config with credentials
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_ID,
@@ -9,20 +13,37 @@ const spotifyApi = new SpotifyWebApi({
 })
 
 // Get an access token
-spotifyApi.clientCredentialsGrant().then(
-    function(data) {
-        spotifyApi.setAccessToken(data.body['access_token']);
-    },
-    function(err) {
-        console.log('Something went wrong!', err);
-    }
-);
+const getAccesToken = async () => {
+    spotifyApi.clientCredentialsGrant().then(
+        function(data) {
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.setRefreshToken(data.body['refresh_token']);
+        },
+        function(err) {
+            console.log('Something went wrong!', err);
+        }
+    );
+}
 
-// The artist ID to get albums from
-const artistId = '4otyLOpxTJ6VdY0EEfjIcS';
+// Access token expires after 1 hour, so request new token if 1 hour has passed since the last token was requested
+const refreshAccessToken = async () => {
+    if (refreshTimer && refreshTimer + 3600 * 1000 < Date.now()) {
+        console.log('Refreshing the token')
+        await getAccesToken();
+        refreshTimer = Date.now();
+    } else {
+        console.log('Access token still valid');
+    }
+}
+
+getAccesToken();
 
 // Get albums from Spotify
 router.get('/', async function(req, res) {
+
+    await refreshAccessToken();
+
+    // Get the Spotify album data
     let response = {};
     await spotifyApi.getArtistAlbums(artistId)
         .then(function(data) {
